eb095f24eb4a728adeed34440b1d04d11720fec5
stormspirit03 <raviraj.gardi@avoma.com>
2024-03-23 20:11:31 +0530

    3rd stage

--- app/main.py
+++ app/main.py
@@ -13,7 +13,9 @@ def main():
     completed_process = subprocess.run([command, *args], capture_output=True)
     sys.stdout.write(completed_process.stdout.decode("utf-8"))
     sys.stderr.write(completed_process.stderr.decode("utf-8"))
-
+    if completed_process.returncode != 0:
+        print(f"Process returned {completed_process.returnecode}")
+        sys.exit(completed_process.returncode)
 
 if __name__ == "__main__":
     main()
